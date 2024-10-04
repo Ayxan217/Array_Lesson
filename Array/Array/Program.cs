fa2960453dfcf23199a462dc7187cfbdc61048d7
ayxan217 <ayxanrm-bp217@code.edu.az>
2024-10-04 11:14:39 +0400

    tasks added

---
+++ Array/Array/Program.cs
@@ -0,0 +1,39 @@
+﻿// arrayın en kiçik elementini tapan program
+
+//int[] arr = { 3, 5, 6, 17, 98, 1, 43, 13, 9 };
+//int smallest = arr[0];
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] < smallest)
+//    {
+//        smallest = arr[i];
+//    }
+//}
+//Console.WriteLine(smallest);
+
+
+
+// array elementlerinin cemini tapan program
+
+//int[] arr = { 3, 5, 6, 17, 98, 1, 43, 13, 9 };
+//int sum = 0;
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    sum += arr[i];
+//}
+//Console.WriteLine(sum);
+
+
+
+// uznluğu 4 den çox olan sözleri tapan program
+
+//string[] words = { "home", "hause", "dog", "cat", "horse", "computer" };
+//for (int i = 0; i < words.Length; i++)
+//{
+//    if (words[i].Length > 4)
+//    {
+//        Console.WriteLine(words[i]);
+//    }
+//}
+
+
